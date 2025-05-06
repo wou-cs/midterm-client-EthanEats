7fb2a9216abf851151bcf72b0ae33be9d3dd94f8
Ethan DeVries <edevries21@mail.wou.edu>
2025-05-05 19:58:13 -0700

    Refactoring done; previous commit only utilized the 'pull all' get, which I figured wasn't the goal.  Now utilizes all 3 different get operations from the api successfully.

--- client.py
+++ client.py
@@ -18,11 +18,13 @@ def get_programmer_by_id(pid):
     :param pid: Unique identifier for the programmer to lookup
     :return: A dictionary containing the matched programmer. Return an empty dictionary if not found
     """
-    response = requests.get(api_url, timeout=5).json()
-    programmers = response["programmers"]
-    programmer = next((p for p in programmers if p["id"] == pid), {})
+    url = api_url + f"/{pid}"
 
-    return programmer
+    try:
+        programmer = requests.get(url, timeout=5).json()
+        return programmer
+    except:
+        return {}
 
 
 def get_full_name_from_first(first_name):
@@ -31,13 +33,12 @@ def get_full_name_from_first(first_name):
     :param first_name:
     :return: A string containing the first and last name of the first programmer in the list of matches.
     """
-    response = requests.get(api_url, timeout=5).json()
+    url = api_url + f"/by_first_name/{first_name}"
+
+
+    response = requests.get(url, timeout=5).json()
     programmers = response["programmers"]
 
-    # 'next' grabs the first element that succeeds at the following:
-    # we're testing each programmer to see if the first name matches the input name (first_name)
-    # if we find one, the succeeding element will be the first and last name appended.
-    # the 'None' at the very end is for defaulting (if there isn't a programmer by the inputted name)
-    programmer = next(((p["first"] + " " + p["last"]) for p in programmers if p["first"].upper() == first_name.upper()), None)
+    programmer = next(((p["first"] + " " + p["last"]) for p in programmers), None)
 
     return programmer
